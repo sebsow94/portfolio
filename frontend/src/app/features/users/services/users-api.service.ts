f0edc094739b01af328a3a88f7de5902cd7c4fdd
Sebastian <sebastianxsowa@gmail.com>
2026-03-31 14:46:02 +0200

    mock server side filter

--- frontend/src/app/features/users/services/users-api.service.ts
+++ frontend/src/app/features/users/services/users-api.service.ts
@@ -6,17 +6,21 @@ import { User } from '../models/user';
   providedIn: 'root',
 })
 export class UsersApiService {
-  getUsers(): Observable<User[]> {
-    return of([
-      {id: 1, name: 'Anna'},
-      {id: 2, name: 'Jan'},
-      {id: 3, name: 'Adrian'},
-      {id: 4, name: 'Michalina'},
-      {id: 5, name: 'Emilia'},
-      {id: 6, name: 'Dawid'},
-      {id: 7, name: 'Helena'},
-      {id: 8, name: 'Ela'},
-      {id: 9, name: 'Henryk'}
-    ]).pipe(delay(500));
+  private readonly usersMock = [
+    { id: 1, name: 'Anna' },
+    { id: 2, name: 'Jan' },
+    { id: 3, name: 'Adrian' },
+    { id: 4, name: 'Michalina' },
+    { id: 5, name: 'Emilia' },
+    { id: 6, name: 'Dawid' },
+    { id: 7, name: 'Helena' },
+    { id: 8, name: 'Ela' },
+    { id: 9, name: 'Henryk' }
+  ];
+
+  getUsers(filter: string): Observable<User[]> {
+    return of(this.usersMock.filter(user =>
+      user.name.toLowerCase().includes(filter.toLocaleLowerCase())
+    )).pipe(delay(500));
   }
 }
